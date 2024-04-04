5d8b9a28cc66f319e3d6a8d3f04bc84fb1c23301
Kantarro <talldirecter@gmail.com>
2024-04-04 10:13:21 -0600

    Completed todo items webapp.

--- web.py
+++ web.py
@@ -16,8 +16,13 @@ st.title('My Todo App')
 st.subheader('This is my todo app.')
 st.write('This app increases your productivity.')
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo) # returns True or False
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo] # deletes from session_state dictionary
+        st.experimental_rerun() # reruns code for checkboxes
 
 st.text_input(label=' ', placeholder='Add a new todo...',
               on_change=add_todo, key='new_todo', label_visibility='collapsed')
